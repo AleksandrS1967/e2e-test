8f4fae79195bf3ac4694b3bc489137c15188c296
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-09-22 22:26:47 +0300

    Refactor

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from src.give_authorization import give_authorization
-from src.added_to_cart import added_to_cart
-from src.submission_form import giv_submission_form
-from src.finish_and_back_to import finish_and_back_to
+import src
 
 
 def app():
-    driver = give_authorization(webdriver, By)
-    added_to_cart(driver, By)
-    giv_submission_form(driver, By)
-    finish_and_back_to(driver, By)
+    driver = src.driver
+    src.give_authorization(driver, By, src)
+    src.added_to_cart(driver, By)
+    src.giv_submission_form(driver, By)
+    src.finish_and_back_to(driver, By)
     driver.close()
     driver.quit()
     print("\nПроизведен выход с платформы...")
     print("\nСценарий тестирования завершен!")
 
-    
+
 if __name__ == "__main__":
     app()
--- src/give_authorization.py
+++ src/give_authorization.py
@@ -1,10 +1,9 @@
-def give_authorization(webdriver, by):
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
+def give_authorization(driver, by, src):
+    driver.get(src.start_dict["link"])
     user_name_input = driver.find_element(by.ID, "user-name")
-    user_name_input.send_keys("standard_user")
+    user_name_input.send_keys(src.start_dict["user_name"])
     password_input = driver.find_element(by.ID, "password")
-    password_input.send_keys("secret_sauce")
+    password_input.send_keys(src.start_dict["password"])
     login_button = driver.find_element(by.ID, "login-button")
     login_button.click()
     print("Авторизация прошла успешно...")
